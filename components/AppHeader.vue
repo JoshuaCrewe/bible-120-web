66b635e0a06be8b745553a8ee0779ffb0c197d98
Joshua <joshua@venncreative.co.uk>
2024-09-09 13:33:37 +0100

    Bring together core app pages and components

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="row justify-between items-center">
-    <Logo />
+    <NuxtLink to="/">
+      <Logo />
+    </NuxtLink>
     <nav>
       <ul class="flex">
         <li><NuxtLink to="/about" class="pr-4 py-4 text-lg block">About</NuxtLink></li>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,9 @@
 <template>
-ABOUT
+  <div class="row">
+    <div class="w-full text-center mt-8">
+      <h1 class="font-black text-4xl">
+        About
+      </h1>
+    </div>
+  </div>
 </template>
